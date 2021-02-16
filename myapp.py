7c8a53055343d259404acd1b2ae57d092bbff300
M Wong <eemtwong@gmail.com>
2021-02-16 21:47:34 +1100

    16-2

--- myapp.py
+++ myapp.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/privacy")
+@app.route("/myprivacy")
 def myprivacy():
     return render_template('privacy.html')
 
