a455b9ee28a97cc6ee12346089429a9f27748883
M Wong <eemtwong@gmail.com>
2021-03-27 04:35:19 +1100

    27-3

--- myapp.py
+++ myapp.py
@@ -11,28 +11,11 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route("/img")
-def img():
-    return('''
-    <a href="/">Home | </a>
-    <a href="/img">Photos | </a>
-    <a href="/blog">Blog</a>
-    <h1>Personal site of M Wong</h1>
-    <h2>Photos</h2>
-    <h3>Australian Catholic University, Strathfield campus</h3>
-    <img src="/static/acu4_flask.jpg">
-    <br><br>
-
-    <h3>UTS Haymarket Building 5, University of Technology Sydney</h3>
-    <img src="/static/uts-bldg5.jpg">
-    <br><br>
-    ''')    
-
-DATABASE_URL = os.environ['DATABASE_URL']
+#DATABASE_URL = os.environ['DATABASE_URL']
 
 #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+#app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL	
 
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL	
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -53,6 +36,34 @@ class Post(db.Model):
 def blog():
     post_data = Post.query.all()
     return render_template("blog.html", post_data = post_data)
+    
+@app.route("/img")
+def img():
+    return('''
+    <a href="/">Home | </a>
+    <a href="/img">Photos | </a>
+    <a href="/blog">Blog</a>
+    <h1>Personal site of M Wong</h1>
+    <h2>Photos</h2>
+    <h3>Australian Catholic University, Strathfield campus</h3>
+    <img src="/static/acu4_flask.jpg">
+    <br><br>
+
+    <h3>UTS Haymarket Building 5, University of Technology Sydney</h3>
+    <img src="/static/uts-bldg5.jpg">
+    <br><br>
+    ''')  
+
+# access blog in local Windows PC
+@app.route("/localblog")
+def locblog():
+    DATABASE = 'db1' # db1 is database file in local PC
+    con = sqlite3.connect(DATABASE)
+    #con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("select * from tb1 order by id desc")
+    rows = cur.fetchall()  
+    return render_template("localblog.html", rows=rows)    
     
 if __name__=="__main__":
     app.run()
