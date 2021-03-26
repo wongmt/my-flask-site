bc6dee01caf865642a3475b520c93c23e70769cc
M Wong <eemtwong@gmail.com>
2021-03-27 04:38:53 +1100

    27-3

--- myapp.py
+++ myapp.py
@@ -11,10 +11,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-#DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL']
 
 #conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-#app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL	
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL	
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
